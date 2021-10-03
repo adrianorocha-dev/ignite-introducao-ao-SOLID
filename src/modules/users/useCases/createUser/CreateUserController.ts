d7cfdbecb99431d1f823fda9a5af43baf51ae64f
Adriano Rocha <adriano.olr@gmail.com>
2021-10-03 19:06:58 -0300

    User controllers

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,15 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { email, name } = request.body;
+
+      const user = this.createUserUseCase.execute({ email, name });
+
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
